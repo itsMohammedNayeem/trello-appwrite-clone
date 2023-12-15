5dc1197c4481c11544a0c89b3bf493688c252280
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-15 15:37:20 +0530

    Fix import statement and add missing type annotations

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,43 +1,43 @@
 "use client";
+import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
 import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 
 function Board() {
-  
-     const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
-       (state) => [
-         state.board,
-         state.getBoard,
-         state.setBoardState,
-         state.updateTodoInDB,
-       ]
-     );
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state: any) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
-     useEffect(() => {
-       getBoard();
-     }, [getBoard]);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
 
-     const handleOnDragEnd = (result: DropResult) => {
-       const { destination, source, type } = result;
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
 
-       if (!destination) return;
+    if (!destination) return;
 
-       return (
-         <DragDropContext onDragEnd={handleOnDragEnd}>
-           <Droppable droppableId="board" direction="horizontal" type="column">
-             {(provided) => (
-               <div
-                 {...provided.droppableProps}
-                 ref={provided.innerRef}
-                 className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
-               >
-                 {/* Creating an array of [todo, inprogress, done, and mapping through this] */}
-               </div>
-             )}
-           </Droppable>
-         </DragDropContext>
-       );
-     };
+    return (
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="board" direction="horizontal" type="column">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            >
+              {/* Creating an array of [todo, inprogress, done, and mapping through this] */}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  };
 }
 
 export default Board;
